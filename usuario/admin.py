c6233b5ef2ff0e7e5fc5302660526bc15b224b48
Deathaholic <borges.trs@gmail.com>
2010-11-29 03:56:03 -0300

    * README: Atualizado para o sprint 2

--- usuario/admin.py
+++ usuario/admin.py
@@ -3,5 +3,6 @@ from LavPython.usuario.models import Usuario
 
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('nome','sobrenome','email')
+    search_fields = ('nome','sobrenome')
 
 admin.site.register(Usuario, UsuarioAdmin)
