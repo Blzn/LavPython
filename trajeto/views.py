2d6c37e0ee3cb3352913add1d9c3374b52a280a3
Deathaholic <borges.trs@gmail.com>
2010-12-12 11:05:26 -0300

    * templates/base.html: Modificado templates para exibir o mapa. TODO: Ajustar a width e height do mapa propriamente

--- trajeto/views.py
+++ trajeto/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils import simplejson
 
+from django.http import HttpResponseRedirect
+
 def trajeto(request):
-    parametrobody = u"onload=\"initialize()\""
+    if request.method == 'POST':
+        testepunk = request.POST.get('dados',False)
+
     return render_to_response(
         'set_trajetos.html',
         locals(),
