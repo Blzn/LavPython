d530033dab1938224bdd9eeb0923dfe3c7ffda2c
Deathaholic <borges.trs@gmail.com>
2010-12-13 19:03:08 -0300

    * trajeto/models.py: Modelo para cadastro criado, funcionando.
    * trajeto/templates/edit_trajeto.html: Template para edição de trajetos adicionado, ainda sendo implementado
    * trajeto/templatetags/custom_filters.py: Foi necessário a criação de alguns filtros
    * trajeto/views.py: Nova view para edição
    * urls.py: Novo url para servir à view de edição

--- trajeto/models.py
+++ trajeto/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Dia(models.Model):
+    data = models.DateField(blank=True,null=True)
+
+    class Meta:
+        ordering = ['-data']
+
+class Trajeto(models.Model):
+    nome = models.CharField(max_length = 120)
+    distancia = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    class Meta:
+        ordering = ['nome']
+
+class Coordenadas(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    trajeto = models.ForeignKey(Trajeto)
+    
+    class Meta:
+        ordering = ['id']
--- trajeto/views.py
+++ trajeto/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.utils import simplejson
+from models import *
 
 from django.http import HttpResponseRedirect
 
+def editar_trajeto(request):
+    trajeto_teste = Trajeto.objects.get(id=6)
+    nome = trajeto_teste.nome
+    distancia = trajeto_teste.distancia
+    cord_teste = trajeto_teste.coordenadas_set.all()
+
+    return render_to_response(
+        'edit_trajeto.html',
+        locals(),
+        context_instance=RequestContext(request))
+
+
+
 def trajeto(request):
     if request.method == 'POST':
-        lol = request.POST.getlist('waypoints')
-        testepunk = lol[0]
+        nome_trajeto = request.POST.get('nome')
+        '''Lembrar de depois tratar o erro para distancia 0
+        Fazer funcao parser dps :B'''
+        distancia_total = request.POST.get('distancia')
+        distancia_total = distancia_total[:-2].replace(',','.')
+        coordenadas_todas = request.POST.getlist('waypoints')
+        
+        novo_trajeto = Trajeto(nome=nome_trajeto, distancia=distancia_total)
+        novo_trajeto.save()
+
+        for coordenada in coordenadas_todas:
+            tupla_coordenada = coordenada[1:-1].split(',')
+            temp_latitude = tupla_coordenada[0]
+            temp_longitude = tupla_coordenada[1]
+            nova_coordenada = Coordenadas(latitude=temp_latitude, longitude= temp_longitude, trajeto=novo_trajeto)
+            nova_coordenada.save()
+
+        
 
     return render_to_response(
         'set_trajetos.html',
