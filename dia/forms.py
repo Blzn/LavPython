0c7e3c1759c9825b40895c5cf3148e459a116241
Deathaholic <borges.trs@gmail.com>
2011-01-28 20:51:51 -0300

    * dia/forms.py: esboço do form

---
+++ dia/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from models import *
+
+DIAS_SEMANA = (
+    (1,'Domingo'),
+    (2,'Segunda'),
+    (3,'Terca'),
+    (4,'Quarta'),
+    (5,'Quinta'),
+    (6,'Sexta'),
+    (7,'Sabado'),
+    )
+
+class FormDia(forms.ModelForm):
+    class Meta:
+        model = Dia
+        exclude = ('carro','dias')
+
+    tipo = forms.ModelChoiceField(choices=TIPO_DIA,widget=forms.Select(),required=True)
+    dias = forms.MultipleChoiceField(choices= DIAS_SEMANA,widget=forms.CheckboxSelectMultiple())
+
+    
+    
