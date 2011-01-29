e4998c0f5994fc19706b7ddaefe613520c2e457c
Deathaholic <borges.trs@gmail.com>
2011-01-28 23:25:55 -0300

    * Progressos (?) com o hack

--- dia/forms.py
+++ dia/forms.py
@@ -1,18 +1,19 @@
 from django import forms
-from django import widgets
 from models import *
 
+
 DIAS_SEMANA = (
-    ('1,','Domingo'),
-    ('2,','Segunda'),
-    ('3,','Terca'),
-    ('4,','Quarta'),
-    ('5,','Quinta'),
-    ('6,','Sexta'),
-    ('7,','Sabado'),
+    (1,'Domingo'),
+    (2,'Segunda'),
+    (3,'Terca'),
+    (4,'Quarta'),
+    (5,'Quinta'),
+    (6,'Sexta'),
+    (7,'Sabado'),
     )
 
-class TextCheckboxSelectMultiple(widgets.CheckboxSelectMultiple):
+
+class TextCheckboxSelectMultiple(forms.CheckboxSelectMultiple):
     def render(self, name, value, **kwargs):
         if isinstance(value, basestring):
             value = value.split(",")
@@ -30,7 +31,7 @@ class FormDia(forms.ModelForm):
         model = Dia
         exclude = ('carro','dias',)
 
-    dias = forms.TextMultiField(choices= DIAS_SEMANA,widget=TextCheckBoxSelectMultiple)
+    dias = TextMultiField(choices=DIAS_SEMANA,)
 
     
 
@@ -43,4 +44,4 @@ class FormDia(forms.ModelForm):
             dia.save()
 
         return dia
-    
+  
