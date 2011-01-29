1b78c5ca1fdde55d526ec7132d00c2630414de9c
Deathaholic <borges.trs@gmail.com>
2011-01-28 22:15:42 -0300

    * dia/forms.py: Tentativa de salvar no form: 80% completo.

--- dia/views.py
+++ dia/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from forms import FormDia
+from django.http import HttpResponseRedirect
+from carro.models import CarroUsuario
 
 def associar(request):
-    form = FormDia()
+    carro = CarroUsuario.objects.get(id=1)
+    if request.method == 'POST':
+        form = FormDia(request.POST)
+        if form.is_valid():
+            novo_dia = form.save(carro)
+            return HttpResponseRedirect(novo_dia.dias)
+    else:
+        form = FormDia()
+    
 
     return render_to_response(
         'associar.html',
