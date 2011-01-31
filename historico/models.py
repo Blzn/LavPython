f6dee918f73587e63a60a58e9eafb8aa28bfda46
Deathaholic <borges.trs@gmail.com>
2011-01-31 20:33:19 -0300

    * historico/models.py: Inicio real do histórico. Model.

--- historico/models.py
+++ historico/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class HistoricoTroca(models.Model):
     carro = models.ForeignKey(CarroUsuario)
-    pecaDe = models.ForeignKey(Peca)
-    saudePeca = models.IntegerField()
-    pecaPara = models.ForeignKey(Peca)
-    
-
+    dePeca = models.ForeignKey(Peca)
+    paraPeca = models.ForeignKey(Peca)
+    saudePeca = models.FloatField()
+    diaDaTroca = models.DateTimeField()
+    quilometragem = models.FloatField()
