f9416a79c1bc6b7ec4e3c8e83146225d6e2ec2ca
Wintermute <wintermute@Wintermute.(none)>
2011-02-02 07:31:51 -0300

    * Editar dia :B não olhe o código, ou você terá pesadelos.

--- dia/urls.py
+++ dia/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('dia.views',
                        url(r'associar/(?P<carro_id>\d+)$','associar',{'classe': 'Dia'},name = 'associar'),
                        url(r'meus_dias/(?P<carro_id>\d+)$','meus_dias',),
+                       url(r'editar_dias/(?P<dia_id>\d+)$','editar_dia',{'classe':'Dia'}, name = 'Editar Dia')
 					   )
--- dia/views.py
+++ dia/views.py
@@ -27,6 +27,27 @@ def associar(request,carro_id,classe):
         'associar.html',
         locals(),
         context_instance=RequestContext(request))
+
+@login_required
+def editar_dia(request,dia_id,classe):
+    dia = Dia.objects.get(id=dia_id)
+    carro = CarroUsuario.objects.get(id=dia.carro_id)
+    if request.method == 'POST':
+        form = FormDia(request.user, request.POST,instance=dia)
+        if form.is_valid():
+            motor = Motor.objects.get(id=carro.motor_id)
+            trajeto = form.cleaned_data['trajeto']
+            consumo = motor.kmLitro * trajeto.distancia
+            novo_dia = form.save(carro,consumo)
+            return HttpResponseRedirect('/')
+    else:
+        form = FormDia(request.user,instance=dia)
+
+    return render_to_response(
+        'associar.html',
+        locals(),
+        context_instance=RequestContext(request))
+
 		
 @login_required
 def meus_dias(request, carro_id):
