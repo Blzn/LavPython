5d65ff51e42958480abbd19c38da9dcf3d2192b1
Deathaholic <borges.trs@gmail.com>
2011-04-25 01:45:26 -0300

    * Adicionado um "controle de transações" desnecessário

--- usuario/views.py
+++ usuario/views.py
@@ -5,22 +5,50 @@ from django.http import HttpResponseRedirect, HttpResponse
 from forms import FormCadastro, FormEdit
 from usuario.models import Usuario
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.db.utils import ConnectionHandler, ConnectionRouter, load_backend, DEFAULT_DB_ALIAS, DatabaseError, IntegrityError
 
+#@transaction.commit_on_success
+#def registrar(request):
+#    if request.method == 'POST':
+#        form = FormCadastro(request.POST)
+#        if form.is_valid():
+#            novo_usuario = form.save()
+#            return HttpResponseRedirect('/')
+#    else:
+#        form = FormCadastro()
+#        
+#    return render_to_response(
+#                            'cadastro/cadastro.html',
+#                            locals(),
+#                            context_instance=RequestContext(request),
+#                            )
+
+
+
+@transaction.commit_manually
 def registrar(request):
     if request.method == 'POST':
         form = FormCadastro(request.POST)
-        if form.is_valid():
-            novo_usuario = form.save()
+        try:
+            if form.is_valid():
+                novo_usuario = form.save(commit=True)
+                novo_usuario.save()
+        except IntegrityError:
+            transaction.rollback()
+        else:
+            transaction.commit()
             return HttpResponseRedirect('/')
     else:
         form = FormCadastro()
-        
+       
     return render_to_response(
                             'cadastro/cadastro.html',
                             locals(),
                             context_instance=RequestContext(request),
                             )
 
+@transaction.commit_on_success
 def edituser(request):
     if request.method == 'POST':
         usuario = Usuario.objects.get(user=request.user)
